45228a6501ebd3e2b7e1e649b895e80eda808f9e
guchen.yh <guchen.yh@alibaba-inc.com>
2023-11-13 14:39:15 +0800

    feat: 删除多余的文件&更新版本号

--- src/commonds/copyAjxDocgen.ts
+++
@@ -1,7 +0,0 @@
-import * as vscode from 'vscode';
-import { ajxDocgen } from './ajxDocgen';
-
-export const copyReactDoc = async (file: vscode.Uri) => {
-  const componentDocs = await ajxDocgen(file);
-  console.log(componentDocs);
-};
